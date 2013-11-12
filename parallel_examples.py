6a749619ec1bcf3409908f83c95e22daea8640b4
Frank Cleary <fcleary@berkeley.edu>
2013-11-12 11:22:39 -0800

    parallel_example now uses for loops and random.random(), working well

--- parallel_examples.py
+++ parallel_examples.py
@@ -5,28 +5,33 @@ Created on Mon Nov 11 15:51:06 2013
 @author: Frank
 """
 
-import numpy
 import pp
 import time
+import random
 
 def randprod(n):
-    "Waste time by creating and multiplying a random n x n matrix"
-    x = numpy.random.rand(n)
-    return numpy.sum(x)
+    "Waste time by creating and inefficiently multiplying an n x n matrix"
+    n = int(n)
+    X = [[random.random() for _ in range(n)] for _ in range(n)]
+    Y = [[0]*n]*n
+    for i in range(n):
+        for j in range(n):
+            Y[i][j] = sum([X[k][i]*X[j][k] for k in range(n)])
+    return Y
 
 if __name__ == "__main__":
-    test_n = [1e8]*4
+    test_n = [300]*4
     start_time = time.time()
     randlist = [randprod(n) for n in test_n]
     elapsed = (time.time() - start_time)
     print "Time for standard processing: %.1f" % elapsed + " s"
 
-    time.sleep(1) # provide a break in windows CPU usage graph    
+    time.sleep(10) # provide a break in windows CPU usage graph    
     
     par_start_time = time.time()
-    ncpus = 4
+    ncpus = 2
     job_server = pp.Server(ncpus)
-    jobs = [job_server.submit(randprod, (n,), (), ("numpy",)) for n in test_n]
+    jobs = [job_server.submit(randprod, (n,), (), ("random",)) for n in test_n]
     sums = [job() for job in jobs]
     par_elapsed = (time.time() - par_start_time)
     print "Time for parallel processing: %.1f" % par_elapsed + " s"
